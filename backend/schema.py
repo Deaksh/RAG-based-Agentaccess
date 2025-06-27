017801c4c68b60b41b388d7b46a63484ecd3a750
Deaksh <Deakshanth.shetty@gmail.com>
2025-06-27 17:27:43 +0530

    Delete backend/schema.py

--- backend/schema.py
+++
@@ -1,8 +0,0 @@
-from qdrant_client import QdrantClient
-
-QDRANT_PATH = "/Users/deakshshetty/Documents/RAG-based-role-access/embeddings/qdrant_local"
-QDRANT_COLLECTION = "finrolebot"
-
-client = QdrantClient(path=QDRANT_PATH)
-schema = client.get_collection(QDRANT_COLLECTION)
-print(schema.payload_schema)
