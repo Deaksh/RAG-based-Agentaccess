5572dba0025cced50180b6fb72ef083694c96d8b
Deaksh <Deakshanth.shetty@gmail.com>
2025-11-10 18:43:34 +0530

    Import ConversationalRetrievalChain from two modules

--- backend/llm_setup.py
+++ backend/llm_setup.py
@@ -4,6 +4,9 @@ from langchain.prompts import PromptTemplate
 from backend.vectorstore_setup import get_vectorstore_for_role
 from langchain.memory import ConversationBufferMemory
 import os
+from langchain_classic.chains import ConversationalRetrievalChain
+from langchain_community.chains import ConversationalRetrievalChain
+
 
 def get_qa_chain(user_role: str):
     retriever = get_vectorstore_for_role(user_role)
