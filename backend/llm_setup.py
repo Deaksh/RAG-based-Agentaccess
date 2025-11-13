46c0580770304bf49ef8b5eb2d846e10ab3cc032
Deaksh <Deakshanth.shetty@gmail.com>
2025-11-13 14:10:12 +0530

    Update llm_setup.py

--- backend/llm_setup.py
+++ backend/llm_setup.py
@@ -5,7 +5,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
-from langchain_community.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferMemory
 from vectorstore_setup import get_vectorstore_for_role
 from dotenv import load_dotenv
 import os
